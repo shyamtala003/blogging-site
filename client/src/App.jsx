c3eefbd959335da5c1db5d5039f3f81ac847933d
shyamtala003 <shyamtala003@gmail.com>
2023-04-30 18:33:56 +0530

    user register route connected with backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,61 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import Layout from "./layout/Layout";
 import IndexPage from "./pages/IndexPage";
 import "./App.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
+// toast message context
+import toastMessageContext from "./context/ToastContext";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [toastMessage, setToastMessage] = useState({});
+
+  useEffect(() => {
+    if (toastMessage.type === "error") {
+      toast.error(toastMessage.message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    }
+  }, [toastMessage]);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout></Layout>}>
-          <Route index element={<IndexPage></IndexPage>}></Route>
-          <Route path="login" element={<Login></Login>}></Route>
-          <Route path="register" element={<Register></Register>}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+      <toastMessageContext.Provider value={{ setToastMessage }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout></Layout>}>
+              <Route index element={<IndexPage></IndexPage>}></Route>
+              <Route path="login" element={<Login></Login>}></Route>
+              <Route path="register" element={<Register></Register>}></Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </toastMessageContext.Provider>
+    </>
   );
 }
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,15 +1,76 @@
+import { useContext, useState } from "react";
 import logo from "../assets/Screenshot__112_-removebg-preview.png";
 import googleLogo from "../assets/google.svg";
+import Axios from "axios";
+
+// toast message context
+import toastMessageContext from "../context/ToastContext";
 
 const Register = () => {
+  // toastmessage context
+  let { setToastMessage } = useContext(toastMessageContext);
+
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function registerUser(e) {
+    e.preventDefault();
+
+    try {
+      let url = import.meta.env.VITE_API_URL;
+      console.log(url);
+      let response = await Axios.post(`${url}/register`, {
+        userName: userName,
+        password: password,
+        email: String(email).toLowerCase(),
+      });
+
+      setUserName("");
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      if (error.response.data.success === false) {
+        setToastMessage({
+          type: "error",
+          message: error.response.data.message,
+        });
+      }
+    }
+  }
+
   return (
     <div className="register">
       <img src={logo} alt="" className="form_logo" />
       <h1 className="form_heading">Register</h1>
-      <form method="post" className="form">
+      <form className="form" onSubmit={registerUser}>
+        <div className="input_group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            className="input_box"
+            name="username"
+            id="username"
+            required
+            value={userName}
+            onChange={(e) => {
+              setUserName(e.target.value);
+            }}
+          />
+        </div>
         <div className="input_group">
           <label htmlFor="email">Email</label>
-          <input type="email" className="input_box" name="email" id="email" />
+          <input
+            type="email"
+            className="input_box"
+            name="email"
+            id="email"
+            required
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
         </div>
         <div className="input_group">
           <label htmlFor="password">Password</label>
@@ -18,6 +79,11 @@ const Register = () => {
             className="input_box"
             name="password"
             id="password"
+            required
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
           />
         </div>
         <button type="submit" className="register_btn">
