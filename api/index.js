adadb34335565af9a537f9db3f12013baab6c786
Shyam Tala <49745830+shyamtala003@users.noreply.github.com>
2023-05-01 17:12:13 +0530

    Update index.js
    cors origin env added

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const cookieParser = require("cookie-parser");
 // set default middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(cookieParser());
 
 // connect app to database
